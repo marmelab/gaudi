d8ff6a79afdb345ff4aa6280827f360da6359053
Emmanuel Quentin <manu.quentin@gmail.com>
2014-01-31 07:01:13 +0100

    Fix tests

--- maestro/maestro_unit_test.go
+++ maestro/maestro_unit_test.go
@@ -152,5 +152,5 @@ applications:
 		docker.EXPECT().Inspect("104").Return([]byte("[{\"ID\": \"104\", \"State\":{\"Running\": true}, \"NetworkSettings\": {\"IPAddress\": \"172.17.0.10\"}}]"), nil),
 	)
 
-	m.Start(true)
+	m.Start()
 }
