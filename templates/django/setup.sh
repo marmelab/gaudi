5ef70774a71e45d1e001b64ed276def2e0d36aef
Emmanuel Quentin <manu.quentin@gmail.com>
2014-06-03 10:06:59 +0200

    Add ror with mod_passenger

--- templates/django/setup.sh
+++ templates/django/setup.sh
@@ -1,9 +1,11 @@
-if [ ! -d "/app/[[ .Container.GetCustomValue "project_name" "project" ]]/[[ .Container.GetCustomValue "app_name" "myapp" ]]" ]; then
-	[[ $projectName := .Container.GetCustomValue "project_name" "project" ]]
-	[[ $appName := .Container.GetCustomValue "app_name" "myapp" ]]
+[[ $dir := .Container.GetFirstMountedDir ]]
+[[ $projectName := .Container.GetCustomValue "project_name" "project" ]]
+[[ $appName := .Container.GetCustomValue "app_name" "myapp" ]]
+
+if [ ! -d "[[$dir]]/[[ $projectName ]]/[[ $appName ]]" ]; then
 
 	# Install django & configure it
-	cd /app
+	cd [[$dir]]
 	django-admin.py startproject [[ $projectName ]] .
 
 	mkdir ./[[ $projectName ]]/[[ $appName]]
@@ -11,7 +13,7 @@ if [ ! -d "/app/[[ .Container.GetCustomValue "project_name" "project" ]]/[[ .Con
 
 	[[ $firstLinked := .Container.FirstLinked]]
 
-	cd /app/[[ $projectName ]]
+	cd [[$dir]]/[[ $projectName ]]
 	sed -i -e "s/'django.db.backends.sqlite3'/'django.db.backends.mysql'/" ./settings.py
 	sed -i -e "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'django',\n\t\t'USER': 'root',\n\t\t'PASSWORD': '',\n\t\t'HOST': os.environ['[[ $firstLinked.Name | ToUpper ]]_PORT_[[ $firstLinked.GetFirstLocalPort]]_TCP_ADDR']/" ./settings.py
 
@@ -19,5 +21,5 @@ if [ ! -d "/app/[[ .Container.GetCustomValue "project_name" "project" ]]/[[ .Con
 	sed -i -e "s/# admin.autodiscover()/admin.autodiscover()/" ./urls.py
 	sed -i -e "s/# url(r'^admin\/', include(admin.site.urls))/url(r'^admin\/', include(admin.site.urls))/" ./urls.py
 
-	echo -e "import os, sys\nbase = os.path.dirname(os.path.dirname(__file__))\nbase_parent = os.path.dirname(base)\nsys.path.append(base)\nsys.path.append(base_parent)\n\n$(cat /app/[[ $projectName ]]/wsgi.py)" > /app/[[ $projectName ]]/wsgi.py
+	echo -e "import os, sys\nbase = os.path.dirname(os.path.dirname(__file__))\nbase_parent = os.path.dirname(base)\nsys.path.append(base)\nsys.path.append(base_parent)\n\n$(cat [[$dir]]/[[ $projectName ]]/wsgi.py)" > [[$dir]]/[[ $projectName ]]/wsgi.py
 fi
---
+++ templates/ror/setup.sh
@@ -0,0 +1,21 @@
+[[ $dir := .Container.GetFirstMountedDir ]]
+[[ $projectName := .Container.GetCustomValue "project_name" "project" ]]
+[[ $version := (.Container.GetCustomValue "version" "1.9.3") ]]
+
+if [ ! -d "[[$dir]]/[[ $projectName ]]" ]; then
+
+	# Install ror
+	/bin/bash -l -c 'gem install rails bundler'
+	cd [[ $dir ]] && /bin/bash -l -c 'rails new [[ $projectName ]] [[ if(.Container.DependsOf "mysql" )]] -d mysql [[ end ]] -T'
+
+	cd [[ $dir ]]/[[ $projectName ]]
+	printf "gem 'execjs'\ngem 'therubyracer'" >> Gemfile
+	/bin/bash -l -c 'bundle install'
+
+	[[ if(.Container.DependsOf "mysql") ]]
+		[[ $db := (.Collection.GetType "mysql")]]
+		sed -i -e "s/host: localhost/host: <%= ENV['DB_PORT_3306_TCP_ADDR'] %>/" [[ $dir ]]/[[ $projectName ]]/config/database.yml
+		sed -i -e "s/host: localhost/host: <%= ENV['DB_PORT_3306_TCP_ADDR'] %>/" [[ $dir ]]/[[ $projectName ]]/config/database.yml
+		/bin/bash -c -l 'rake db:migrate'
+	[[ end ]]
+fi
